bfea77820d44621d4b0cc1bf83d5bcb402e6b25d
Adrian Coria <adrian.c.coria@gmail.com>
2023-05-01 16:08:26 -0700

    get user input

--- rps.js
+++ rps.js
@@ -2,3 +2,14 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return choice;
 }
+
+function playRound(playerSelection,computerSelection) {
+    
+}
+
+function getUserInput() {
+    let input = prompt("What is your choice?").toLowerCase();
+    return input;
+}
+
+// console.log(getUserInput());
