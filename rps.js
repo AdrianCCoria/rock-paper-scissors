0bb2f2cd604f781b23d137ee35adf8a280a4aa1f
Adrian Coria <adrian.c.coria@gmail.com>
2023-05-15 17:21:39 -0700

    add gui

--- rps.js
+++ rps.js
@@ -13,71 +13,78 @@ function getComputerChoice() {
     }
 }
 
-// console.log(getComputerChoice());
+function playRound(e) {
+    const playerSelection = e.target.id;
+    if (!playerSelection) return;
+    const computerSelection = getComputerChoice();
 
-function playRound(playerSelection,computerSelection) {
+    let result;
+
    if (playerSelection === computerSelection) {
-    console.log("TIE!");
+    result = "TIE!";
    } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            console.log("Computer chose paper!");
-            console.log("Paper beats Rock - You Lose!");
+            result = "Computer chose paper! Paper beats Rock";
             computerWins += 1;
         } else {
-            console.log("Computer chose scissors!");
-            console.log("Rock beats scissors - You Win!");
+            result = "Computer chose scissors! Rock beats scissors"
             playerWins += 1;
         }
    } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            console.log("Computer chose rock!");
-            console.log("Paper beats Rock - You Win!");
+            result = "Computer chose rock! Paper beats Rock";
             playerWins += 1;
         } else {
-            console.log("Computer chose scissors!");
-            console.log("Scissors beat Paper - You Lose!");
+            result = "Computer chose scissors! Scissors beat Paper";
             computerWins += 1;
         } 
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            console.log("Computer chose rock!");
-            console.log("Rock beats Scissors - You Lose!");
+            result = "Computer chose rock! Rock beats Scissors";
             computerWins += 1;
         } else {
-            console.log("Computer chose paper!");
-            console.log("Scissors beat Paper - You Win!");
+            result = "Computer chose paper! Scissors beat Paper";
             playerWins += 1;
         } 
     } else {
-        alert("Not a valid choice - Try Again");
-        playRound(getUserInput(),getComputerChoice());
+        result = "Not a valid choice - try again";
     }
-}
 
-function getUserInput() {
-    let input = prompt("Enter: rock, paper, or scissors").toLowerCase();
-    return input;
+    const resultDiv = document.createElement('div');
+    resultDiv.textContent = result;
+
+    const parentDiv = document.querySelector('#results');
+    parentDiv.innerHTML = '';
+    parentDiv.appendChild(resultDiv);
+
+    displayScore();
+
+    checkWinner();
 }
 
 function displayScore() {
-    console.log("Player: ", playerWins);
-    console.log("Computer: ", computerWins);
+    const scoreDiv = document.createElement('div');
+    scoreDiv.textContent = `Player Wins: ${playerWins} - Computer Wins: ${computerWins}`;
+    
+    const parentDiv = document.querySelector('#score');
+    parentDiv.innerHTML = '';
+    parentDiv.appendChild(scoreDiv);
 }
 
-// playRound(getUserInput(),getComputerChoice());
-// console.log(getUserInput());
+function checkWinner() {
+    if (playerWins === 5 || computerWins === 5) {
+        const winner = (playerWins > computerWins) ? "You Win the Game!" : "You Lose the Game!";
+        const winnerDiv = document.createElement('div');
+        winnerDiv.textContent = winner;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log("Round ", i + 1);
-        playRound(getUserInput(),getComputerChoice());
-        displayScore();
-    }
-    if (playerWins > computerWins) {
-        alert("YOU WON THE GAME");
-    } else {
-        alert("YOU LOST - GAME OVER");
+        document.body.appendChild(winnerDiv);
+
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach(button => {
+            button.removeEventListener('click', playRound);
+            button.disabled = true;
+        });
     }
 }
 
-game();
+window.addEventListener("click", playRound);
